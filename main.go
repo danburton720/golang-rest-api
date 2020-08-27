bc03aace25abb374c7ddaac79b1f6e9cdeb74e72
Dan Burton <danburton720@hotmail.com>
2020-08-27 07:59:00 +0100

    added mux to allow us to specify what verbs can use what endpoints

--- main.go
+++ main.go
@@ -5,6 +5,8 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+
+	"github.com/gorilla/mux"
 )
 
 type Article struct {
@@ -29,9 +31,12 @@ func homePage(w http.ResponseWriter, r *http.Request) {
 }
 
 func handleRequests() {
-	http.HandleFunc("/", homePage)
-	http.HandleFunc("/articles", allArticles)
-	log.Fatal(http.ListenAndServe(":8081", nil))
+
+	myRouter := mux.NewRouter().StrictSlash(true)
+
+	myRouter.HandleFunc("/", homePage)
+	myRouter.HandleFunc("/articles", allArticles)
+	log.Fatal(http.ListenAndServe(":8081", myRouter))
 }
 
 func main() {
